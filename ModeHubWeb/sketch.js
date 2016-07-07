107e316f9df1b54fe12d66076771b0bf9b09df55
SeungBum Kim <picxenk@gmail.com>
2016-07-07 15:26:56 +0900

    add p5.js libraries

--- ModeHubWeb/sketch.js
+++ ModeHubWeb/sketch.js
@@ -1,21 +1,47 @@
 var socket;
 var modebit;
-var bgColor = color(150, 220, 10);
+var bgColor;
+var d;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	bgColor = color(150, 220, 10);
 	background(bgColor);
-	socket = io.connect('http://192.168.1.100:8080');
-	
-	socket.on('modebit',
-		function(data) {
-			modebit = data;
-		}
-	);
-
+	modebit = 0;
+// 	socket = io.connect('http://192.168.1.100:8080');
+// 	
+// 	socket.on('modebit',
+// 		function(data) {
+// 			modebit = data;
+// 		}
+// 	);
+	d = 200;
 }
 
 function draw() {
 	background(bgColor);
-	println(modebit);
+	noStroke();
+	
+	
+	if (modebit == 1) {
+		fill(color(0, 200, 0));	
+		d = min(200, d + 9);
+	} else {
+		fill(color(200, 220, 200));
+		d = max(100, d - 3);
+	}
+	
+	ellipse(width/2, height/2, d, d);
+	
+	
+	simulateBits(20);
+
+}
+
+function simulateBits(t) {
+	if (frameCount % t < (t/2)) {
+			modebit = 1;
+	} else {
+		modebit = 0;
+	}
 }
